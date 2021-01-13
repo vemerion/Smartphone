2eb2d2cfbcef595f818818ad1aaf20faea566647
vemerion <vemerion@gmail.com>
2021-01-13 13:18:15 +0100

    Cleanup

--- src/main/java/mod/vemerion/smartphone/phone/app/ForRedditApp.java
+++ src/main/java/mod/vemerion/smartphone/phone/app/ForRedditApp.java
@@ -418,7 +418,6 @@ public class ForRedditApp extends App {
 		@Override
 		protected void connect() throws Exception {
 			if (token == null || System.currentTimeMillis() - timestamp > HOUR) {
-				System.out.println("REQUESTING TOKEN" + " " + token + " " + (System.currentTimeMillis() - timestamp));
 				HttpURLConnection connection = (HttpURLConnection) new URL(TOKEN_URL).openConnection();
 				connection.setRequestMethod("POST");
 				byte[] encodedAuth = Base64.getEncoder().encode(APP_ID.getBytes(StandardCharsets.UTF_8));
@@ -435,13 +434,11 @@ public class ForRedditApp extends App {
 				os.close();
 
 				Integer responseCode = connection.getResponseCode();
-				System.out.println("Response Code : " + responseCode);
 
 				if (responseCode == HttpURLConnection.HTTP_OK) {
 					JsonObject json = new JsonParser().parse(getResponse(connection)).getAsJsonObject();
 					token = JSONUtils.getString(json, "access_token");
 					timestamp = System.currentTimeMillis();
-					System.out.println("TOKEN: " + token);
 				}
 			}
 
@@ -452,11 +449,9 @@ public class ForRedditApp extends App {
 				connection.setRequestMethod("GET");
 
 				int responseCode = connection.getResponseCode();
-				System.out.println("Response Code : " + responseCode);
 
 				if (responseCode == HttpURLConnection.HTTP_OK) {
 					String response = getResponse(connection);
-					System.out.println(response);
 					setData(response);
 				}
 			}
@@ -481,11 +476,9 @@ public class ForRedditApp extends App {
 				connection.setRequestMethod("GET");
 
 				int responseCode = connection.getResponseCode();
-				System.out.println("Response Code : " + responseCode);
 
 				if (responseCode == HttpURLConnection.HTTP_OK) {
 					String response = getResponse(connection);
-					System.out.println(response.toString());
 					setData(response);
 				}
 			}
